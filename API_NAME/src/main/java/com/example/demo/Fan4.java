0b634bbaa68f9f547fed9164cd9b6760916c7307
Azizbek Husainov <ahusainov.2005@gmail.com>
2025-10-14 15:20:53 -0400

    Added api

---
+++ API_NAME/src/main/java/com/example/demo/Fan4.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fan4 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fan4.class, args);
+	}
+
+}
