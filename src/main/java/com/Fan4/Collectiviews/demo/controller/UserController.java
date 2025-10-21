a77334c77c767257339348d79fb4c63bfe887529
Jerico Corneja <jerico.corneja20@gmail.com>
2025-10-20 22:23:08 -0400

    Add user controller n service

---
+++ src/main/java/com/Fan4/Collectiviews/demo/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.Fan4.Collectiviews.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(path = "user") // Base URL might use in bruno?
+public class UserController {
+    
+    // 
+    
+
+}
