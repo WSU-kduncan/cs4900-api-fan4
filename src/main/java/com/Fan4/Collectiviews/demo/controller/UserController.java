d73d0fee10639ec63b83f3a890a3aa8ffc474da1
Jerico Corneja <jerico.corneja20@gmail.com>
2025-10-20 23:43:24 -0400

    Create class and add TODOs

--- src/main/java/com/Fan4/Collectiviews/demo/controller/UserController.java
+++ src/main/java/com/Fan4/Collectiviews/demo/controller/UserController.java
@@ -1,16 +1,36 @@
 package com.Fan4.Collectiviews.demo.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import com.Fan4.Collectiviews.demo.model.User;
+import com.Fan4.Collectiviews.demo.service.UserService;
 
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping(path = "user") // Base URL might use in bruno?
+@RequestMapping(
+    path = "user",  // Base URL might use in bruno?
+    produces = MediaType.APPLICATION_JSON_VALUE,
+    consumes = MediaType.APPLICATION_JSON_VALUE) 
 public class UserController {
     
-    // 
+    // Interface between controller and repository
+    private UserService userService;
     
+    // TODO: implement findAll controller method
+    @GetMapping
+    public List<User> getAllUsers(){
+        return userService.findAll();
+    }
+
+    // TODO: implement findById
+
+    // TODO: implement findByString
+
 
 }
--- src/main/java/com/Fan4/Collectiviews/demo/service/UserService.java
+++ src/main/java/com/Fan4/Collectiviews/demo/service/UserService.java
@@ -1,6 +1,11 @@
 package com.Fan4.Collectiviews.demo.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.Fan4.Collectiviews.demo.model.User;
+import com.Fan4.Collectiviews.demo.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -8,4 +13,13 @@ import lombok.RequiredArgsConstructor;
 @Service
 public class UserService {
     
+    // Interface to repository
+    private UserRepository userRepository;
+
+    /* Business Logic Methods */
+
+    public List<User> findAll() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+    }
 }
